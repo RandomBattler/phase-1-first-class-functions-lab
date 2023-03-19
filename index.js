e8da82947b87ab01cc18e9c5b8a767c0bf600090
Steve Romero <romero.steve.a@gmail.com>
2023-03-19 14:49:26 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(d){
+    return [d[0], d[1]];
+}
+
+function returnLastTwoDrivers(d){
+    
+    return [d[d.length -2], d[d.length -1]];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(mult){
+    return function(fare){
+        return fare * mult;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, func){
+    return func(drivers);
+}
